5e2656a5d864ede2e644e094fa59d561be4481ea
gorbasho <37026147+gorbasho@users.noreply.github.com>
2018-05-01 23:15:31 -0500

    title

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FunWithGifs
+
+This app allows the user to search the GIPHY gif database and displays the results. Searches are saved in a tab, and the user may switch between the tabs without losing their contents. GIFs are loaded by default in a paused state, and may be played by clicking on them.
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,10 @@
 
 var topics = ["Cake", "Cookies", "Brownies", "Ice\u00a0Cream"];
 
+// Variable to track offset of results returned when user requests to load more gifs
+var offset = 0;
+
+
 function initialTabs() {
     $(".list-group-item-action").remove();
     $("#nav-tabContent").empty();
@@ -11,33 +15,53 @@ function initialTabs() {
 }
 initialTabs();
 
+
+// Generates new subject tab and new content box for subject
 function makeTab(input) {
     var newTab = $("<a>");
     newTab.addClass("list-group-item list-group-item-action");
-    newTab.attr({"id":input, "href":`#list-${input}`, "role":"tab"});
+    newTab.attr({"id":input, "href":`#list-${input}`, "role":"tab", "data-searched":false});
     newTab.text(input);
     $("#list-tab").append(newTab);
     
     var newTabContent = $("<div>");
     newTabContent.addClass("tab-pane fade");
     newTabContent.attr({"id":`list-${input}`, "role":"tabpanel"});
-    newTabContent.text("Test test test test test test test test test test test test test");
     $("#nav-tabContent").append(newTabContent);
+
+    
 }
 
 
 
+//$('#list-tab a').on('click', runQuery);
 
-$('#list-tab a').on('click', runQuery);
+$('div').on('click', "#list-tab a", checkClick);
 
-function runQuery(e) {
+// Function for persistance across tab clicks- Once a tab has performed search and loaded gifs, subsequent clicks will not redo search and erase previous results
+function checkClick(e) {
+    e.stopPropagation();
     e.preventDefault()
-    $(this).tab('show') 
-    // Grabbing and storing the data-animal property value from the button
-    var subject = $(this).attr("id");
+    console.log($(this).attr("data-searched"));
+    if ($(this).attr("data-searched") === "false") {
+        runQuery(this, 0);
+    } else {
+        console.log("no search");
+        $(this).tab('show') 
+    }
 
-    // Constructing a queryURL using the animal name
-    var queryURL = `https://api.giphy.com/v1/gifs/search?q=${subject}&limit=10&api_key=H6iVL2wZikmz4JFyIELiwNQtVY5RVvAr`;
+}
+
+
+function runQuery(selectedTab, offsetinput) {
+    $(selectedTab).tab('show') 
+
+    // Update tab status to already searched
+    $(selectedTab).attr("data-searched", true);
+    var subject = $(selectedTab).attr("id");
+
+    // Constructing a queryURL using the input subject
+    var queryURL = `https://api.giphy.com/v1/gifs/search?q=${subject}&limit=10&offset=${offsetinput}&api_key=H6iVL2wZikmz4JFyIELiwNQtVY5RVvAr`;
 
     // Performing an AJAX request with the queryURL
     $.ajax({
@@ -50,50 +74,111 @@ function runQuery(e) {
 
         console.log(response);
         // storing the data from the AJAX request in the results variable
-        var searchResults = response.data;
+        searchResults = response.data;
 
         // Looping through each result item
         for (var x = 0; x < searchResults.length; x++) {
 
-          // Creating and storing a div tag
-          var gifDiv = $("<div>");
+            // Creating and storing a div tag
+            var gifDiv = $("<div>");
 
-          // Creating a paragraph tag with the result item's rating
-          var p = $("<p>").text("Rating: " + searchResults[x].rating);
+            // Split input string into individual letters
+            var ratingSplit = searchResults[x].rating.split();
 
-          // Creating and storing an image tag
-          var resultGif = $("<img>");
-          // Setting the src attribute of the image to a property pulled off the result item
-          resultGif.attr({"src": searchResults[x].images.fixed_height_still.url, "data-state": "still", "data-count": x});
-          resultGif.addClass("gif");
+            // For each letter in string..
+            for (var i = 0; i < ratingSplit.length; i++) {
+                // ..capitalize letter
+                ratingSplit[i] = ratingSplit[i].toUpperCase();
+            }
+            // Join words back into single string
+            var rating = ratingSplit.join(" ");
+            console.log(rating);
 
-          // Appending the paragraph and image tag to the animalDiv
-          gifDiv.append(p);
-          gifDiv.append(resultGif);
+            // Creating a paragraph tag with the result item's rating
+            var p = $("<p>").text("Rating: " + rating);
 
-          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-          console.log($(`#list-${subject}`));
-          $("#list-" + subject).prepend(gifDiv);
+            // Creating and storing an image tag
+            var resultGif = $("<img>");
+            // Setting the src attribute of the image to a property pulled off the result item
+            resultGif.attr({"src": searchResults[x].images.fixed_height_still.url, "data-state": "still", "data-still": searchResults[x].images.fixed_height_still.url, "data-animate": searchResults[x].images.fixed_height.url});
+            resultGif.addClass("gif img-fluid");
+
+            // Appending the paragraph and image tag to the gifDiv
+            gifDiv.append(p);
+            gifDiv.append(resultGif);
+
+            // Prependng the gifDiv to the HTML page
+            console.log($(`#list-${subject}`));
+            $("#list-" + subject).append(gifDiv);
         }
 
-        $(".gif").on("click", function() {
-            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-            var state = $(this).attr("data-state");
-            var count = $(this).attr("data-count");
-            console.log(state);
-            console.log(count);
-            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-            // Then, set the image's data-state to animate
-            // Else set src to the data-still value
-            if (state === "still") {
-              $(this).attr("src", searchResults[count].images.fixed_height.url);
-              $(this).attr("data-state", "animate");
-            } else {
-              $(this).attr("src", searchResults[count].images.fixed_height_still.url);
-              $(this).attr("data-state", "still");
-            }
-        });
+        // Create Load More button at bottom of results
+        var loadMoreButton = $("<button>");
+            loadMoreButton.addClass("btn btn-primary");
+            loadMoreButton.attr("id", "moreGifButton");
+            loadMoreButton.text("Load more");
+            $(`#list-${subject}`).append(loadMoreButton);
+        
+
     });
+};
+
+function displayBox() {
+    var gifDiv = $("<div>");
+
+    // Split input string into individual letters
+    var ratingSplit = searchResults[x].rating.split();
+
+    // For each letter in string..
+    for (var i = 0; i < ratingSplit.length; i++)
+    {   // ..capitalize letter
+        ratingSplit[i].toUpperCase();
+    }
+    // Join words back into single string
+    var rating = ratingSplit.join(" ");
+
+    // Creating a paragraph tag with the result item's rating
+    var p = $("<p>").text("Rating: " + rating);
+
+    // Creating and storing an image tag
+    var resultGif = $("<img>");
+    // Setting the src attribute of the image to a property pulled off the result item
+    resultGif.attr({"src": searchResults[x].images.fixed_height_still.url, "data-state": "still", "data-still": searchResults[x].images.fixed_height_still.url, "data-animate": searchResults[x].images.fixed_height.url});
+    resultGif.addClass("gif img-fluid");
+
+    // Appending the paragraph and image tag to the gifDiv
+    gifDiv.append(p);
+    gifDiv.append(resultGif);
+
+    // Prependng the gifDiv to the HTML page
+    console.log($(`#list-${subject}`));
+    $("#list-" + subject).append(gifDiv);
+}
+
+
+
+
+// Listener and function for pausing/playing gifs
+$("div").on("click", ".gif", gifClick);
+
+function gifClick(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    // Create vars for animated and still gif URLs
+    var state = $(this).attr("data-state");
+    var still = $(this).attr("data-still");
+    var animate = $(this).attr("data-animate");
+    console.log(state);
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      $(this).attr("src", animate);
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", still);
+      $(this).attr("data-state", "still");
+    }
 };
 
 
@@ -102,10 +187,10 @@ $("#searchButton").on("click", function(event) {
     // event.preventDefault() prevents the form from trying to submit itself.
     // We're using a form so that the user can hit enter instead of clicking the button if they want
     event.preventDefault();
+    event.stopPropagation();
 
     // Grab the text from input box, trim beginning/ending whitespace
     var search = $("#searchForm").val().trim();
-    console.log(search);
 
     // Split input string into individual words
     var searchSplit = search.split(" ");
@@ -119,18 +204,28 @@ $("#searchButton").on("click", function(event) {
     search = searchSplit.join(" ");
     // Replace spaces in string with Unicode NBSP
     search = search.replace(/\s+/g, '\u00a0');
-    console.log(search);
     // Input added to topic array
     topics.push(search);
 
     makeTab(search);
-    
 
     // Reset search box to placeholder
     $("#searchForm").val("");
 
-    // Create listener for topic tabs
-    $('#list-tab a').on('click', runQuery);
     // Auto-search the user's new search
     $('#' + search).trigger("click");
 });
+
+
+// Listener and function for Load More Gifs button
+$(document.body).on("click", "#moreGifButton", loadMore); 
+function loadMore(e) {
+    e.preventDefault();
+    // Set query offset to 10 so that results return unique gifs
+    offset += 10;
+
+    runQuery($("a.active"), offset);
+    // Hide old button, new one will be generated in runQuery function at bottom of page
+    $("#moreGifButton").remove();
+   
+};
